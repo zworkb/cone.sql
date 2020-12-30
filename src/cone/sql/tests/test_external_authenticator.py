10a3f4482152bd8affdbab5adfadbd1a56cea39d
Philipp Auersperg-Castell <philk@bluedynamics.com>
2020-12-30 09:39:43 +0100

    test_authenticator_global_authenticate

--- src/cone/sql/tests/test_external_authenticator.py
+++ src/cone/sql/tests/test_external_authenticator.py
@@ -4,6 +4,7 @@ import os
 
 from cone.app import ugm_backend, security
 from cone.app.authenticators.firebase import FirebaseAuthenticator
+from cone.app.model import AppRoot
 from firebase_admin import auth
 from node.ext.ugm.interfaces import IAuthenticator, IUsers
 from node.tests import NodeTestCase
@@ -16,7 +17,7 @@ from zope import component
 from cone.sql import testing
 
 FIREBASE_WEB_API_KEY = "AIzaSyDqQThSScLrwBybYW5m22rZSYILELPsDz8"
-rest_api_url = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
+FIREBASE_API_URL = f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword"
 
 certkey_raw = b'eyJ0eXBlIjogInNlcnZpY2VfYWNjb3VudCIsICJwcm9qZWN0X2lkIjogIndpbGxob2x6ZW4tMjkzMjA4IiwgInByaXZhdGVfa2V5X2lkIjogIjZlZjNiODE4MzJlNTdkMjdmZjNkODQ1ODkzYzUyNjA2ODg2MmJmZmIiLCAicHJpdmF0ZV9rZXkiOiAiLS0tLS1CRUdJTiBQUklWQVRFIEtFWS0tLS0tXG5NSUlFdlFJQkFEQU5CZ2txaGtpRzl3MEJBUUVGQUFTQ0JLY3dnZ1NqQWdFQUFvSUJBUUR0WW5Nb091TVQ1NW1aXG5ldXFwdWhCSGhDcEpVdkJ0YkNHaXAycnh4YkNoUEVnQUdWRjI3Z1JibWlYRXFvczFxcGlzcmtZY3BXSXM3ZXNrXG5PU2xHanJzQ3J5cjJaTVFtbFdzeFVYWFpnMHllZTR2OXpZWHZOczh1dldJYjVmVWlOZEZKZDdYR3N6Z2VvSTdSXG5LWCsrcUhZUTFjUnJ6VFNLUVg2YVQ3RnpiTDdnaUNmNXl6bVdEZlF4QXpMbkhqQ21IbWE1WVkrR2tBeVNMRE8yXG5JZ2lHSlVWZzR5YmZSSVMrY2h2c3owQmpteEIwdGV3aW9zRGlGNVhHU2M2UU1NSGJhb3BTcGtWUVZ2cVJzOXdTXG53VGlEVCtBaXB0NHByK1hZM0ZBOHBYRndWaDlpRzF5UVI0T2NDS0gwdWZMRVg2eE5RYW0rL3JMNUtjR2pyeVB0XG5wZ3NkdWZnTEFnTUJBQUVDZ2dFQVNXNFJRaW9KRVNDc3BrbmI2Zm1vekg2SkdCcGtUZXpMY01XSjh0VGNvTnVGXG5TQ2FNeVJXeVdtV1I5a1pzL29KUmJPVVJ3ZzIwazBKYllONmZzbmczU0FySVp1UmZ1ckd3c1ZaSjlxbERXNmpLXG44bzBQTjZLRUx5aWMveFZBb1Q3MWlOelRhVzRKMTR1QjhoaDhzQXpyVEtUYmVCOUhkOWJ2b3lzd3NJdzhkVGFBXG5acjA0QmorNE9tUUxPTERTTk93c0Q5TE9QbC9YSnduTjdlMXlHVjhXQkpDcWJUSEJZU2lwS0EwOUJMU1NZQTV6XG5ESzhWNGJ0VlFid1V3N1JSckpvQ0tZc3FIb2t0Nm42NG1KcHJnMy8zK3dzSy9oU3NUQnNxOWNoRW90Rm9OQjM3XG5ndGZrcU5IdFFPR3BzengzSXkwYXpkcUdXRlR2NHF3aW9udVJoVDRtQ1FLQmdRRDRtYkp3WHdxY2M1MlpDdHpoXG44R21DbEhqV3pQOXpTdmR4UmNnNmdUMDFuU2p1cEhjRkFaa1F4VmhQcnFTNTQvVnE3d0VOdWhuNzJVbzZHKzZZXG52OGJFZVpqbEFCS2dIcjBmWDNyTXZwZGZCQWZSWkNWK0JCdkdKa0pRb3lSdFlFU211TS96ajlRMmhXT3c5NXBrXG4rRnFKaStldjRwbVdkamZJZjJIN01lalRzd0tCZ1FEMGMwb3U1VUFXK2R6RFlNcUpZRXJ2eGtvZ1dwT05wOWtwXG53NXZBL1V1Nm9hRXc4RmI2ZlJ1V3YwRDY3SEVtYzR1MlZRTU1QZ3JwU1FVdUx1RWtRT2NKMzdweVN3OStUREFmXG5wN09NMlMyZlcybHpPN1JkRExIQVVad0JZM0RjSyt6Z2hyemduN2VOdW4vZ3NUaDl4TmkwZFY2c1VzYWtrekM4XG40UXVGUVk3K1NRS0JnQ2puS1B2aU4xMjhqQjZsTWVpZTVNMU9YbitCSlRxMEIvaUVNY2ltZ1FxMFBBcmYxcEZsXG5UT2o4ZjJaRTV1ZUxBVk5CMWR1TFZrRWc1RnloUWVXeUVDTTltcEY3TEpWWjJXWXpFSUpsallSZHBtZW1ZYURWXG5UamlSRFBJMWxZWEZoQ1ltWEZqSHZuUG1DSndTY1QzUnV4dk1nQ2RwVWpuMlk0RkNyRDFuWGRhUEFvR0FRTzVlXG54aFllNy90dXFIY05WWEErZDNJMFBtUFF6Yy9IMzFBV0R5MDQ4Rm4rZHdGZ0dTa2lLdWlpV2dSMENjS25XY2M3XG5DQVZ5NElTRTI3K1lEZ2t2VlhBWUZrUFoyYkpqRmdWMXEvUUVLbEFkc240cGFvMzBxU0VFVDFvYW9HRUtpcGsvXG5rbXJhVnNzekxIdm1oWUhFRGlzZStxY0NITFQ4UFMrSjM5dUtJQUVDZ1lFQTJwOFByTk56SXV1cFk5K0tBVEZtXG5Nb0xHK0RiS25adWppR0toNEZLNlZaeWJXTDhnM1BFSzNKOUZVK3dzZml6OXE4TDE4Ni8rUUNPZmlKMWpZRU9KXG4rdnhtbnhoNHcvMmpxRUZidVJxOXEzczMrcXRBcjZVZHByV0FHUjNzblRreTlVUkhKRXFjSDBoN1RHWFZmWkxXXG5vVWFQMFFEc2FwVVFZeVFlVVlDSVFXQT1cbi0tLS0tRU5EIFBSSVZBVEUgS0VZLS0tLS1cbiIsICJjbGllbnRfZW1haWwiOiAiZmlyZWJhc2UtYWRtaW5zZGstaWZ6YmFAd2lsbGhvbHplbi0yOTMyMDguaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCAiY2xpZW50X2lkIjogIjExMzMxMzgzNjcwNDU0MDY3MTgxNiIsICJhdXRoX3VyaSI6ICJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20vby9vYXV0aDIvYXV0aCIsICJ0b2tlbl91cmkiOiAiaHR0cHM6Ly9vYXV0aDIuZ29vZ2xlYXBpcy5jb20vdG9rZW4iLCAiYXV0aF9wcm92aWRlcl94NTA5X2NlcnRfdXJsIjogImh0dHBzOi8vd3d3Lmdvb2dsZWFwaXMuY29tL29hdXRoMi92MS9jZXJ0cyIsICJjbGllbnRfeDUwOV9jZXJ0X3VybCI6ICJodHRwczovL3d3dy5nb29nbGVhcGlzLmNvbS9yb2JvdC92MS9tZXRhZGF0YS94NTA5L2ZpcmViYXNlLWFkbWluc2RrLWlmemJhJTQwd2lsbGhvbHplbi0yOTMyMDguaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20ifQ=='
 certkey = json.loads(base64.b64decode(certkey_raw))
@@ -38,14 +39,26 @@ class FirebaseTest(NodeTestCase):
         self.user = self.fb_user()
 
     def tearDown(self):
+        super().tearDown()
         self.layer.tearDown()
+
+        # dispose remnants of the ugm setup
+        root = AppRoot()
+        settings_factories = root['settings'].factories
+        for k in settings_factories.keys():
+            del settings_factories[k]
+
+        root_factories = root.factories
+        for k in ["users", "groups"]:
+            del root_factories[k]
 
     def firebase_init(self):
         import firebase_admin
         from firebase_admin import credentials
 
         self.cred = credentials.Certificate(certkey)
-        self.app = firebase_admin.initialize_app(self.cred)
+        if not firebase_admin._apps:
+            self.app = firebase_admin.initialize_app(self.cred)
 
     def fb_user(self):
         from firebase_admin.auth import UserNotFoundError
@@ -104,7 +117,7 @@ class FirebaseTest(NodeTestCase):
         ugm = ugm_backend.ugm
         users = ugm.users
 
-        adapter = component.provideAdapter(
+        component.provideAdapter(
             factory=lambda x:
                 FirebaseAuthenticator(ugm.users, FIREBASE_WEB_API_KEY),
             adapts=[IUsers],
